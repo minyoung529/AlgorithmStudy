a8078f12aa81e26854d0d2469fed484c86fba121
minyoung529 <dlalsdud102613@gmail.com>
2022-07-11 00:01:52 +0900

    8_IronRod

---
+++ 10_IronRod.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main()
+{
+	string input;
+	int count = 0;
+	int result = 0;
+
+	cin >> input;
+
+	for (int i = 0; i < input.size(); i++)
+	{
+		if (input[i] == '(')
+		{
+			++count;
+		}
+		else
+		{
+			--count;
+
+			if (i > 0 && input[i - 1] == '(')
+			{
+				result += count;
+			}
+			else
+			{
+				result++;
+			}
+		}
+	}
+
+	cout << result;
+}
