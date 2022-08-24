f2b65243049fd2b258f27c4615fe402af891ccdc
minyoung529 <dlalsdud102613@gmail.com>
2022-08-24 22:27:42 +0900

    Create 7_Prime_Number.cpp

---
+++ Math/7_Prime_Number.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+
+	int m, n;
+	cin >> m >> n;
+	
+	for(int i = m; i <= n; i++)
+	{
+		
+	}
+}
