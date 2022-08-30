72844634b2eef277307dc524c72d36f722c42254
minyoung529 <dlalsdud102613@gmail.com>
2022-08-30 19:59:54 +0900

    Solve Math\7_Prime_Number

--- Math/7_Prime_Number.cpp
+++ Math/7_Prime_Number.cpp
@@ -3,13 +3,31 @@ using namespace std;
 
 int main()
 {
-	ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
-
 	int m, n;
+	int min = 0, sum = 0;
+
 	cin >> m >> n;
-	
-	for(int i = m; i <= n; i++)
+
+	for (int i = m; i <= n; i++)
 	{
-		
+		int count = 0;
+
+		for (int j = 1; j <= i; j++)
+		{
+			if (count > 2) break;
+			if (i % j == 0) ++count;
+		}
+
+		if (count == 2)
+		{
+			if (sum == 0) min = i;
+			sum += i;
+		}
 	}
+
+	if (sum == 0)
+		cout << -1;
+
+	else
+		cout << sum << endl << min;
 }
