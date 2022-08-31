14312de38997fe8b5fa83d4c25044533c090543a
minyoung529 <dlalsdud102613@gmail.com>
2022-08-31 11:17:23 +0900

    Solve Math\11_LCM

--- Math/10_Sieve_of_Eratosthenes.cpp
+++ Math/10_Sieve_of_Eratosthenes.cpp
@@ -10,16 +10,19 @@ int main()
 
 	for (int i = 2; i <= n; i++)
 	{
+		// 이미 체크한 숫자면 continue
 		if (check[i]) continue;
 
 		int p = i;
 
 		while (p <= n)
 		{
+			// 이미 체크한 숫자면 체크하지 않는다
 			if (!check[p])
 			{
 				check[p] = true;
 
+				// K번째 지울 수일 때
 				if (--k == 0)
 				{
 					cout << p << endl;
@@ -27,6 +30,7 @@ int main()
 				}
 			}
 
+			// p의 배수를 순서대로 (ex) 3, 6, 9...)
 			p += i;
 		}
 	}
