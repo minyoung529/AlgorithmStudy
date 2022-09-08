f1ed706fbbe3d2ceb70f00f4690b1b5d835bfad6
minyoung529 <dlalsdud102613@gmail.com>
2022-09-08 13:45:35 +0900

    그래프 DFS, BFS 탐색 구현

--- 자료구조 교과과목 연습파일/Adjacency_Matrix_Graph.cpp
+++ 자료구조 교과과목 연습파일/Adjacency_Matrix_Graph.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
-#define MAX_VTXS	256
+#define MAX_VTXS	50
 
 class AdjMatGraph
 {
 protected:
 	int						size;
-	vector<char>			vertices;
-	vector<vector<int>>		adj;
+	char					vertices[MAX_VTXS];
+	int						adj[MAX_VTXS][MAX_VTXS];
 	const int				MAX_SIZE;
 
 public:
 	AdjMatGraph() : MAX_SIZE(MAX_VTXS)
-	{
-		reset();
-	}
-
-	AdjMatGraph(int maxSize) : MAX_SIZE(maxSize)
 	{
 		reset();
 	}
@@ -66,22 +62,69 @@ public:
 	{
 		size = 0;
 
-		vertices.resize(MAX_SIZE);
-
-		adj.resize(MAX_SIZE);
-
-		for (int i = 0; i < MAX_SIZE; i++)
-			adj[i].resize(MAX_SIZE);
-
 		for (int i = 0; i < MAX_SIZE; i++)
 			for (int j = 0; j < MAX_SIZE; j++)
 				setEdge(i, j, 0);
 	}
 };
 
+class SearchGraph : public AdjMatGraph
+{
+	bool visited[MAX_VTXS];
+
+public:
+	void resetVisited()
+	{
+		for (int i = 0; i < size; i++)
+		{
+			visited[i] = false;
+		}
+	}
+
+	bool isLinked(int u, int v) { return getEdge(u, v) != 0; }
+
+	void DFS(int v)
+	{
+		visited[v] = true;
+		cout << vertices[v] << " ";
+
+		for (int i = 0; i < size; i++)
+		{
+			if (isLinked(v, i) && !visited[i])
+			{
+				DFS(i);
+			}
+		}
+	}
+
+	void BFS(int v)
+	{
+		visited[v] = true;
+
+		queue<char> q;
+		q.push(v);
+
+		while (!q.empty())
+		{
+			int i = q.front();
+			cout << vertices[i] << " ";
+			q.pop();
+
+			for (int j = 0; j < size; j++)
+			{
+				if (isLinked(j, i) && !visited[j])
+				{
+					q.push(j);
+					visited[j] = true;
+				}
+			}
+		}
+	}
+};
+
 int main()
 {
-	AdjMatGraph g(50);
+	SearchGraph g;
 
 	for (int i = 0; i < 4; i++)
 	{
@@ -96,4 +139,14 @@ int main()
 
 	cout << "인접 행렬로 표현한 그래프" << endl;
 	g.display();
+
+	cout << "DFS 탐색 = > ";
+	g.resetVisited();
+	g.DFS(0);
+
+	cout << endl;
+
+	cout << "BFS 탐색 = > ";
+	g.resetVisited();
+	g.BFS(0);
 }
