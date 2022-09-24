942b7b8c9bf6dde866a16af9fe1545007ce74e1b
minyoung529 <dlalsdud102613@gmail.com>
2022-09-24 21:58:04 +0900

    Solve Tomato

---
+++ 소스.cpp
@@ -0,0 +1,31 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	for (int i = 0; i < 5; i++)
+//	{
+//		for (int j = 5; j > i; j--)
+//		{
+//			cout << " ";
+//		}
+//
+//		for (int k = 1; k <= i * 2 + 1; k++)
+//		{
+//			cout << "*";
+//		}
+//
+//		cout << '\n';
+//	}
+//
+//	for (int i = 0; i < 4; i++)
+//	{
+//		for (int j = 0; j < i + 2; j++)
+//			cout << " ";
+//
+//		for (int k = 1; k <= (3 - i) * 2 + 1; k++)
+//			cout << "*";
+//
+//		cout << '\n';
+//	}
+//}
