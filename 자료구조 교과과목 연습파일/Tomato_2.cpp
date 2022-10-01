f44d610a32c4f4fd38b61f913308e7586d5a10e8
minyoung529 <dlalsdud102613@gmail.com>
2022-10-02 00:14:00 +0900

    Solve & Review DP1\12_Combination

---
+++ 자료구조 교과과목 연습파일/Tomato_2.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ELEMENT(p)	[p.d][p.h][p.w]
+
+struct pos { int d, h, w; };
+
+void Tomato(pos);
+
+int dx[6] = { 0,1,0,-1,0,0 };
+int dy[6] = { 1, 0,-1,0,0,0 };
+int dz[6] = { 0, 0,0,0,1,-1 };
+
+int width, height, depth, curTomato = 0, tomatoSum = 0, answer = 0;
+int field[100][100][100];
+bool visited[100][100][100];
+queue<pos> q;
+
+int main()
+{
+	cin >> width >> height >> depth;
+
+	for (int i = 0; i < depth; i++)
+	{
+		for (int j = 0; j < height; j++)
+		{
+			for (int k = 0; k < width; k++)
+			{
+				pos temp = { i,j,k };
+				cin >> field ELEMENT(temp);
+
+				if (field ELEMENT(temp) == 1)
+					q.push(temp);
+
+				else if (field ELEMENT(temp) == 0)
+					tomatoSum++;
+			}
+		}
+	}
+
+	while (!q.empty())
+	{
+		pos pos = q.front();
+
+		if (field ELEMENT(pos) > 0 && !visited ELEMENT(pos))
+		{
+			Tomato(pos);
+		}
+
+		q.pop();
+		answer = field ELEMENT(pos);
+	}
+
+	if (curTomato == tomatoSum)
+		cout << answer - 1;
+	else
+		cout << -1;
+}
+
+void Tomato(pos position)
+{
+	for (int i = 0; i < 6; i++)
+	{
+		pos nPos = position;
+		nPos.d += dz[i];
+		nPos.h += dy[i];
+		nPos.w += dx[i];
+
+		if (nPos.d < 0 || nPos.h < 0 || nPos.w < 0) continue;
+		if (nPos.d >= depth || nPos.h >= height || nPos.w >= width)continue;
+
+		if (visited ELEMENT(nPos) || field ELEMENT(nPos) != 0) continue;
+
+		field ELEMENT(nPos) = field ELEMENT(position) + 1;
+		q.push(nPos);
+		curTomato++;
+	}
+}
