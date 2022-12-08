3351a93a3ac67a4c156e0e94eaf4b04cdeae672d
minyoung529 <dlalsdud102613@gmail.com>
2022-12-09 08:14:44 +0900

    Revert "solve make_1"
    This reverts commit fbe8651db41a1624578392654a637e047d76f011.

--- DataStructure2/Finding_PW.cpp
+++ DataStructure2/Finding_PW.cpp
@@ -1,26 +1,26 @@
-//#include<iostream>
-//#include<unordered_map>
-//using namespace std;
-//
-//int main()
-//{
-//	int userLen, orderLen;
-//	unordered_map<string, string> map;
-//	cin >> userLen >> orderLen;
-//
-//	for (int i = 0; i < userLen; i++)
-//	{
-//		string site, pw;
-//		cin >> site >> pw;
-//
-//		map[site] = pw;
-//	}
-//
-//	for (int i = 0; i < orderLen; i++)
-//	{
-//		string site;
-//		cin >> site;
-//
-//		cout << map[site] << '\n';
-//	}
-//}
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+int main()
+{
+	int userLen, orderLen;
+	unordered_map<string, string> map;
+	cin >> userLen >> orderLen;
+
+	for (int i = 0; i < userLen; i++)
+	{
+		string site, pw;
+		cin >> site >> pw;
+
+		map[site] = pw;
+	}
+
+	for (int i = 0; i < orderLen; i++)
+	{
+		string site;
+		cin >> site;
+
+		cout << map[site] << '\n';
+	}
+}
