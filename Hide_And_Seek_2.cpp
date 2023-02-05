55288f0c1f3aefe8407276c718c957f584c6f63e
minyoung529 <dlalsdud102613@gmail.com>
2023-02-06 01:31:41 +0900

    Solve 숨바꼭질 2

--- Hide_And_Seek_2.cpp
+++ Hide_And_Seek_2.cpp
@@ -1,55 +1,56 @@
-//#include<iostream>
-//#include<queue>
-//using namespace std;
-//
-//#define MAX 100000
-//bool visited[100001];
-//int start, endV, answer = 0;
-//
-//void set(int f, int s, queue<pair<int, int>>& q)
-//{
-//	if (f < 0 || f > endV + 1 || visited[f]) return;
-//
-//	visited[f] = true;
-//	q.push({ f , s });
-//}
-//
-//int main()
-//{
-//	cin >> start >> endV;
-//
-//	queue<pair<int, int>> q;
-//	int ans = 0, same = 0;
-//	q.push({ start, 0 });
-//
-//	if (start < endV)
-//	{
-//		while (!q.empty())
-//		{
-//			pair<int, int> top = q.front();
-//
-//			if (top.first == endV)
-//			{
-//				if (ans == 0 || ans == top.second)
-//				{
-//					ans = top.second;
-//					same++;
-//				}
-//				else break;
-//			}
-//
-//			q.pop();
-//
-//			set(top.first + 1, top.second + 1, q);
-//			set(top.first - 1, top.second + 1, q);
-//			set(top.first * 2, top.second + 1, q);
-//		}
-//	}
-//	else
-//	{
-//		cout << start - endV << '\n' << 1;
-//		return 0;
-//	}
-//
-//	cout << ans << '\n' << same;
-//}
+#include<iostream>
+#include<queue>
+#include<string>
+using namespace std;
+
+struct node
+{
+	int f, s;
+	string str;
+};
+
+#define MAX 100000
+bool visited[100001];
+int start, endV, answer = 0;
+
+void set(int f, int s, queue<node>& q, string str = "")
+{
+	if (f < 0 || f > endV + 1 || visited[f]) return;
+
+	visited[f] = true;
+	str += (to_string(f)) + ' ';
+	q.push({ f , s, str });
+}
+
+int main()
+{
+	cin >> start >> endV;
+
+	queue<node> q;
+
+	q.push({ start, 0, to_string(start) + ' ' });
+
+	if (start < endV)
+	{
+		while (!q.empty())
+		{
+			node top = q.front();
+			if (top.f == endV)break;
+			q.pop();
+
+			set(top.f + 1, top.s + 1, q, top.str);
+			set(top.f - 1, top.s + 1, q, top.str);
+			set(top.f * 2, top.s + 1, q, top.str);
+		}
+	}
+	else
+	{
+		cout << start - endV << '\n';
+
+		for (int i = start; i >= endV; i--)
+			cout << i << ' ';
+		return 0;
+	}
+
+	cout << q.front().s << '\n' << q.front().str;
+}
